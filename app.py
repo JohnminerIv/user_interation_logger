6d0461d76de3c7317430758ad02681020b5e4882
JohnminerIv <john.douglas.miner.iv@gmail.com>
2020-09-25 16:53:56 -0800

    [EDIT] I edited app.py to add in firebase and log user interactions.

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ from tensorflow import keras
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("auth_key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 application = app = Flask(__name__)
 api = Api(app, version='1.0', title='MNIST Classification', description='CNN for Mnist')
@@ -31,7 +38,8 @@ class CNNPrediction(Resource):
     """Uploads your data to the CNN"""
     @api.doc(parser=single_parser, description='Upload an mnist image')
     def post(self):
-        single_parser.parse_args().file.save('posted_img.png')
+        args = single_parser.parse_args()
+        args.file.save('posted_img.png')
         img = Image.open('posted_img.png')
         image_red = img.resize((28, 28))
         image = img_to_array(image_red)
@@ -39,7 +47,15 @@ class CNNPrediction(Resource):
         x = x/255
         out = model.predict(x)
         r = str(np.argmax(out[0]))
-        return {'prediction': r}
+
+        data = {
+            'date': str(dt.datetime.now()),
+            'file_name': args.file.filename,
+            'result': r
+        }
+        db.collection('predictions').document(args.file.filename).set(data)
+
+        return {'prediction': r, 'name': args.file.filename}
 
 
 if __name__ == '__main__':
