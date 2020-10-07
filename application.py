a9ecf3b7c66eede91ebf73c2be56e2d2f04b467b
JohnminerIv <john.douglas.miner.iv@gmail.com>
2020-10-07 13:54:30 -0800

    [ADD] classes.npy edited code to use raptor maps images, zipped pushed to aws and google firebase.

--- application.py
+++ application.py
@@ -13,9 +13,11 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import LabelEncoder
 
 import firebase_admin
 from firebase_admin import credentials, firestore
+import sys
 
 cred = credentials.Certificate("auth_key.json")
 firebase_admin.initialize_app(cred)
@@ -23,14 +25,16 @@ db = firestore.client()
 
 
 application = app = Flask(__name__)
-api = Api(app, version='1.0', title='MNIST Classification', description='CNN for Mnist')
-ns = api.namespace('Make_School', description='Methods')
+api = Api(app, version='1.0', title='Solar panel anomaly', description='CNN for solar panel images')
+ns = api.namespace('Raptor maps project', description='Methods')
 
 single_parser = api.parser()  # parsing args is one of the benefits of Flask-RESTPlus
 single_parser.add_argument('file', location='files',
                            type=FileStorage, required=True)
 
 model = load_model('model.h5')
+encoder = LabelEncoder()
+encoder.classes_ = np.load('classes.npy')
 
 
 @ns.route('/prediction')
@@ -41,17 +45,25 @@ class CNNPrediction(Resource):
         args = single_parser.parse_args()
         args.file.save('posted_img.png')
         img = Image.open('posted_img.png')
-        image_red = img.resize((28, 28))
+        print(sys.getsizeof(img))
+
+        image_red = img.resize((40, 24))
         image = img_to_array(image_red)
-        x = image.reshape((1, 784))
+        x = image.reshape((1, 40, 24, 1))
         x = x/255
         out = model.predict(x)
-        r = str(np.argmax(out[0]))
+        r = np.argmax(out[0])
+        r = encoder.inverse_transform([r])[0]
+        print(sys.getsizeof(image))
+        ls = list(image.reshape(-1))
+        ls = ' '.join(map(str, ls))
+        print(sys.getsizeof(ls))
 
         data = {
             'date': str(dt.datetime.now()),
             'file_name': args.file.filename,
-            'result': r
+            'result': r,
+            'image': ls
         }
         db.collection('predictions').document(args.file.filename).set(data)
 
